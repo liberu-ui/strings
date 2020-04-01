cf7db1be68d12da535e598808e6dc96838f4ce22
mishu <mishu@xtelecom.ro>
2020-04-01 15:04:09 +0300

    fixes exports

--- src/strings.js
+++ src/strings.js
@@ -1,28 +1,27 @@
 const regEx = new RegExp(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g);
 
-const snake = string => {
-    return string.match(this.regEx)
-        .map(word => word.toLowerCase())
-        .join('_');
-}
+const snake = string => string.match(regEx)
+    .map(word => word.toLowerCase())
+    .join('_');
 
-const slug = (string, slug = '-') => {
-    return string.match(this.regEx)
-        .map(word => word.toLowerCase())
-        .join(slug);
-}
+const slug = (string, slug = '-') => string.match(regEx)
+    .map(word => word.toLowerCase())
+    .join(slug);
 
-
-const pascal = string => {
-    return string.match(this.regEx)
-        .map(word => word.toUpperCase())
-        .join('');
-}
+const pascal = string => string.match(regEx)
+    .map(word => word.toUpperCase())
+    .join('');
 
 const camel = string => {
     string = pascal(string);
 
-    return string.charAt(0).toLowerCase() +  string.slice(1); 
-}
+    return string.charAt(0).toLowerCase() + string.slice(1);
+};
 
-export default { snake, slug, pascal, camel };
+const lcwords = string => string.match(regEx)
+    .map(word => word.toLowerCase())
+    .join(' ');
+
+export {
+    snake, slug, pascal, camel, lcwords,
+};
