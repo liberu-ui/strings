3db4a82acb8ff7f8b5ee29f848176fd1b0ee815c
mishu <mishu@xtelecom.ro>
2021-07-03 10:48:04 +0300

    refactors strings

--- src/strings.js
+++ src/strings.js
@@ -1,42 +1,30 @@
-const diff = (first, second) => first.split('')
+const diff = (first = '', second = '') => first.split('')
     .filter((char, index) => char !== second.charAt(index))
     .join('');
 
 const regEx = new RegExp(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g);
 
-const snake = string => string
-    ? string.match(regEx)
-        .map(word => word.toLowerCase())
-        .join('_')
-    : '';
+const snake = (string = '') => string.match(regEx)
+    .map(word => word.toLowerCase())
+    .join('_');
 
-const slug = (string, slug = '-') => string
-    ? string.toLowerCase()
-        .match(regEx)
-        .join(slug)
-    : '';
+const slug = (string = '', slug = '-') => string.toLowerCase()
+    .match(regEx)
+    .join(slug);
 
-const pascal = string => string
-    ? string.match(regEx)
-        .map(word => word.toUpperCase())
-        .join('')
-    : '';
+const pascal = (string = '') => string.match(regEx)
+    .map(word => word.toUpperCase())
+    .join('');
 
-const camel = string => lcfirst(pascal(string));
+const camel = (string = '') => lcfirst(pascal(string));
 
-const lcfirst = string => string
-    ? string.charAt(0).toLowerCase() + string.slice(1)
-    : '';
+const lcfirst = (string = '') => string.charAt(0).toLowerCase() + string.slice(1);
 
-const lcwords = string => string
-    ? string.match(regEx)
-        .map(word => word.toLowerCase())
-        .join(' ')
-    : '';
+const lcwords = (string = '') => string.match(regEx)
+    .map(word => word.toLowerCase())
+    .join(' ');
 
-const ucfirst = string => string
-    ? string.charAt(0).toUpperCase() + string.slice(1)
-    : '';
+const ucfirst = (string = '') => string.charAt(0).toUpperCase() + string.slice(1);
 
 export {
     diff, snake, slug, pascal, camel, lcfirst, lcwords, ucfirst,
